bf1e65ba20b839cec5d01cb9834e21b4d934181f
Ubuntu <ubuntu@ip-172-31-46-120.us-west-2.compute.internal>
2013-07-09 10:24:56 +0000

    Changing web.js to read file from byte streamwq

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
