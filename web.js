a03e39a232e3bf3d540fe5956ecb0f066915f04f
Ubuntu <ubuntu@ip-172-31-46-120.us-west-2.compute.internal>
2013-07-19 16:28:13 +0000

    Css changes to boot strapper page

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buffer = fs.readFileSync('index.html');
   response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
